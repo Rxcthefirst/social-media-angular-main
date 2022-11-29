419485afe4f60caac7a59aed2bad08c955c6514b
HyungJoo Yoon <hyungjooyoon@disroot.org>
2022-11-29 09:09:43 -0800

    changed urls to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   withCredentials: true,
-  baseUrl: "http://35.92.196.181:8081",
+  baseUrl: "http://52.37.182.192:8081",
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:4200',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   withCredentials: true,
-  baseUrl: "http://52.37.182.192:8081",
+  baseUrl: "localhost:8081",
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:4200',
