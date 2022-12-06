611daebd1604cf5d7c6785db6debf5c7b6dc2d6f
HyungJoo Yoon <hyungjooyoon@disroot.org>
2022-12-06 12:09:26 -0800

    added nevironment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    withCredentials: true,
+    baseUrl: "http:localhost:8081",
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:4200',
+    },
+  };
