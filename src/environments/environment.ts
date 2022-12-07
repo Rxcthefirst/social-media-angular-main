be9908bcc52cb46f40bfb10017154ecec96c8b6b
Rxcthefirst <81718883+Rxcthefirst@users.noreply.github.com>
2022-12-07 09:49:25 -0800

    finalized

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     withCredentials: true,
-    baseUrl: "http:localhost:8081",
+    baseUrl: "http://localhost:8081",
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': 'http://localhost:4200',
