ec797b60543a0403187ead363e12f50401eda6f5
Rxcthefirst <81718883+Rxcthefirst@users.noreply.github.com>
2022-12-12 09:27:43 -0800

    feature completed

--- src/app/components/shared/vote-button/vote-button.component.ts
+++ src/app/components/shared/vote-button/vote-button.component.ts
@@ -18,6 +18,8 @@ export class VoteButtonComponent implements OnInit {
   votePayload: Vote;
   upvoteColor: string;
   downvoteColor: string;
+  downVote: boolean = false
+  upVote: boolean = false
 
   constructor(
     private voteService: VoteService,
@@ -37,6 +39,7 @@ export class VoteButtonComponent implements OnInit {
 
   ngOnInit(): void {
     this.updateVoteDetails();
+    this.checkVoteStatus();
   }
 
   
@@ -56,6 +59,7 @@ export class VoteButtonComponent implements OnInit {
     let newVote = new Vote(0, this.votePayload.voteType, this.post, this.authService.currentUser)
     this.voteService.vote(newVote).subscribe(() => {
       this.updateVoteDetails();
+      this.checkVoteStatus();
     }, error => {
       throw(error);
     });
@@ -65,6 +69,29 @@ export class VoteButtonComponent implements OnInit {
     this.postService.getPost(this.post.id).subscribe(post => {
       this.post = post;
     });
+  }
+
+  checkVoteStatus = () => {
+    let res = this.voteService.checkVote(this.post.id)
+    res.subscribe((data) => {
+      if (!data) {
+        this.downVote = false
+        this.upVote = false
+        console.log(`No data for ${this.post.id}`)
+      }
+
+      if (data.voteType.toString()==="DOWNVOTE") {
+        this.downVote = true
+        this.upVote = false
+        console.log(`The status on post id: ${this.post.id} is DOWNVOTE and this.downvote is ${this.downVote}`)
+      }
+
+      if (data.voteType.toString()==="UPVOTE") {
+        this.downVote = false
+        this.upVote = true
+        console.log(`The status on post id: ${this.post.id} is UPVOTE and this.upVote is ${this.upVote}`)
+      }
+    })
   }
     
 }
--- src/app/services/vote.service.ts
+++ src/app/services/vote.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Vote } from '../models/Vote';
 import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,17 @@ export class VoteService {
 
   private voteUrl: string = `${environment.baseUrl}/vote`
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   vote(votePayload: Vote): Observable<any> {
     return this.http.post(`${this.voteUrl}`, votePayload, {headers: environment.headers, withCredentials: environment.withCredentials});
+  }
+
+  findVote(id: number): Observable<any> {
+    return this.http.get(`${this.voteUrl}/getOne/${id}`, {headers: environment.headers, withCredentials: environment.withCredentials});
+  }
+
+  checkVote(id: number): Observable<Vote> {
+    return this.http.get<Vote>(`${this.voteUrl}/one/${this.authService.currentUser.id}&${id}`, {headers: environment.headers, withCredentials: environment.withCredentials});
   }
 }
